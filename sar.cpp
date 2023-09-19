fb59ca25e6dff560eebf91892a9fb6c172a30ac3
cool-cellist <144144238+cool-cellist@users.noreply.github.com>
2023-09-18 18:40:55 -1000

    Faster Testing Results
    -Removed printLists() calls
    -All sorts are done in one go for faster results

--- sar.cpp
+++ sar.cpp
@@ -2,23 +2,46 @@
 #include "listResources.hpp"
 #include "sortingAlgs.hpp"
 
+class Stopwatch {
+    std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    public:
+    void restart() {
+        start = std::chrono::steady_clock::now();
+    }
+
+    int64_t duration() {
+        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()-start).count();
+    }
+};
+
 int main() {
     //prints list
-    std::vector<int> list = generateList(-100, 100, 100);
-
-    //prints unsorted list
-    std::cout << "\n\nUNSORTED:\n";
-    printList(list);
-
+    Stopwatch sp = Stopwatch();
+    std::vector<int> list = generateList(-100, 100, 1000);
     //times the sorting algorithm
-    auto start = std::chrono::steady_clock::now();
-    insertionSort(list);
-
-    //prints the sorted list
-    std::cout << "\n\nSORTED:\n";
-    printList(list);
-
+    sp.restart();
+    selectionSort(list);
     //prints the time taken
-    auto dur = std::chrono::steady_clock::now()-start;
-    std::cout << "\nMICROSECONDS: " << std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
+    std::cout << "\nSELECTION SORT MICROSECONDS: " << sp.duration() <<"\n\n";
+
+
+
+    //prints list
+    list = generateList(-100, 100, 1000);
+    //times the sorting algorithm
+    sp.restart();
+    insertionSort(list);
+    //prints the time taken
+    std::cout << "\nINSERTION SORT MICROSECONDS: " << sp.duration() <<"\n\n";
+
+
+
+    //prints list
+    list = generateList(-100, 100, 1000);
+    //times the sorting algorithm
+    sp.restart();
+    bubbleSort(list);
+    //prints the time taken
+    std::cout << "\nBUBBLE SORT MICROSECONDS: " << sp.duration() <<"\n\n";
 }
