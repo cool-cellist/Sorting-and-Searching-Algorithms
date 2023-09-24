8a3ac9a8198a6140e5ac9b9f9832f12888ac979b
cool-cellist <144144238+cool-cellist@users.noreply.github.com>
2023-09-24 10:19:30 -1000

    QUICKSORT!!!!!!
    -Added the quicksort algorithm
    -For it to work, it needs two functions
      -Partitioning, which takes a range (low -> pivot) and returns the final pivot index
       -The main function, used to recurse and partition around the final pivot indexes
    -Quicksort is generally faster than merge sort, and requires O(logn) time rather than O(n) time like merge sort.  It has a worse case of n^2, though.  Merge sort is consistent with nlogn.

--- sortingAlgs.hpp
+++ sortingAlgs.hpp
@@ -6,21 +6,23 @@
 #include <functional>
 
 void selectionSort(std::vector<int>& list) {
+    int temp = 0;
     for (int i=0; i<list.size(); i++) {
         int* minimum = &list.at(i);
         for (int j=i; j<list.size(); j++) {
             if (*minimum>list.at(j)) {minimum = &list.at(j);}
         }
-        int temp = *minimum;
+        temp = *minimum;
         *minimum = list.at(i);
         list.at(i) = temp;
     }
 }
 
 void insertionSort(std::vector<int>& list) {
+    int temp = 0;
     for (int i=1; i<list.size(); i++) {
         for (int j=i; ( (j>0) && (list.at(j)<list.at(j-1)) ); j--) { 
-            int temp = list.at(j);
+            temp = list.at(j);
             list.at(j) = list.at(j-1);
             list.at(j-1) = temp;
         }
@@ -28,10 +30,11 @@ void insertionSort(std::vector<int>& list) {
 }
 
 void bubbleSort(std::vector<int>& list) {
+    int temp = 0;
     for (int i=list.size(); i>0; i--) {
         for (int j=0; j<i-1; j++) {
             if ((list.at(j))>(list.at(j+1))) {
-                int temp = list.at(j);
+                temp = list.at(j);
                 list.at(j) = list.at(j+1);
                 list.at(j+1) = temp;
             }
@@ -76,4 +79,33 @@ std::vector<int> mergeSort(std::vector<int>& list) {
     
     return merge(lowerhalf, upperhalf);
 
+}
+
+int partition (std::vector<int>& list, int low, int pivot) {
+    int temp = 0;
+    int i=low-1, j=low;
+    while (j<pivot) {
+        if (list.at(j)<=list.at(pivot)) {
+            temp = list.at(++i);
+            list.at(i) = list.at(j);
+            list.at(j) = temp;
+        }
+        ++j;
+    }
+
+    ++i;
+    temp = list.at(i);
+    list.at(i) = list.at(pivot);
+    list.at(pivot) = temp;
+
+    return i;
+}
+
+void quickSort(std::vector<int>& list, int low=0, int high=-1) {
+    if (high<0) {high=list.size()-1;}
+    if (low>=high || list.size() <= 1) {return;}
+    
+    int pivotIndex = partition(list, low, high);
+    quickSort(list, low, pivotIndex-1);
+    quickSort(list, pivotIndex+1, high);
 }
