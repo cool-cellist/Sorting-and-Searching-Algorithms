660bdc6466d5998ad464e59e9f019513af3c3547
cool-cellist <144144238+cool-cellist@users.noreply.github.com>
2023-09-30 17:32:02 -1000

    Minor Update to searchingAlgs.hpp
    Only changed the list parameter in linearSearch() to pass by reference and be consistent

--- searchingAlgs.hpp
+++ searchingAlgs.hpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int linearSearch(std::vector<int> list, target) {
+int linearSearch(std::vector<int>& list, target) {
   for (int i=0; i<list.size(); i++) {
     if (list.at(i)==target) {
       return i;
