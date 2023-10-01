c272c0eddb81e3978bac4bbddefe9c8ded672c25
cool-cellist <144144238+cool-cellist@users.noreply.github.com>
2023-09-30 17:27:31 -1000

    Optimized and renamed sar.cpp to sorting.cpp
    Uses new function in listResouces.hpp to generate an ordered set
    Now tests lists with items 1000, 2000, 3000, 4000, and 5000 for better graphing

--- sorting.cpp
+++ sorting.cpp
@@ -1,19 +1,5 @@
-#include <chrono>
 #include "listResources.hpp"
 #include "sortingAlgs.hpp"
-
-class Stopwatch {
-    std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
-
-    public:
-    void restart() {
-        start = std::chrono::steady_clock::now();
-    }
-
-    int64_t duration() {
-        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()-start).count();
-    }
-};
 
 Stopwatch sp = Stopwatch();
 int64_t timeAlgo(void (*func)(std::vector<int>&), int items) {
@@ -39,42 +25,30 @@ int64_t timeAlgo(void (*func)(std::vector<int>&, int, int), int items) {
 
 int main() {
 
-    int items[] = {100, 1000, 10000};
-    int tests = 3;
+    std::vector<int> items = generateOrderedIncrementedSet(1000, 5000, 1000);
+    int tests = items.size();
 
     std::cout << "\nSELECTION SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(selectionSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(selectionSort, items[i]) << ", ";}
     std::cout << "\n\n";
 
     std::cout << "\nINSERTION SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(insertionSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(insertionSort, items[i]) << ", ";}
     std::cout << "\n\n";
 
     std::cout << "\nBUBBLE SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(bubbleSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(bubbleSort, items[i]) << ", ";}
     std::cout << "\n\n";
 
     std::cout << "\nMERGE SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(mergeSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(mergeSort, items[i]) << ", ";}
     std::cout << "\n\n";
 
     std::cout << "\nQUICK SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(quickSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(quickSort, items[i]) << ", ";}
     std::cout << "\n\n";
 
     std::cout << "\nHEAP SORT MICROSECONDS: ";
-    for (int i=0; i<tests; i++) {
-        std::cout << timeAlgo(heapSort, items[i]) << ", ";
-    }
+    for (int i=0; i<tests; i++) {std::cout << timeAlgo(heapSort, items[i]) << ", ";}
     std::cout << "\n\n";
 }
