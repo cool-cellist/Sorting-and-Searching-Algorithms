eaf8eaf768903e0e992a4b78bb261ddff1453594
cool-cellist <144144238+cool-cellist@users.noreply.github.com>
2023-10-17 19:43:38 -0700

    Update searchingAlgs.hpp

--- searchingAlgs.hpp
+++ searchingAlgs.hpp
@@ -1,6 +1,7 @@
 #include <vector>
-#include <cmath>
+#include <cmath> //for jump search, which needs sqrt computation
 
+//sequentially checks items to see if it is the target
 int linearSearch(std::vector<int>& list, int target) {
   for (int i=0; i<list.size(); i++) {
     if (list.at(i)==target) {
@@ -10,6 +11,7 @@ int linearSearch(std::vector<int>& list, int target) {
   return -1;
 }
 
+//splits the items in half so that it finds the range in which it is in, and repeats the process until it narrows down to the target
 int binarySearch(std::vector<int>& list, int target) {
   int left = 0, right = list.size()-1;
   int middle = 0;
@@ -28,6 +30,7 @@ int binarySearch(std::vector<int>& list, int target) {
   return -1;
 }
 
+//basically binarySearch but splits the list in 3
 int ternarySearch(std::vector<int>& list, int target) {
   int left = 0, right = list.size()-1;
   int mid1 = 0, mid2 = 0;
@@ -55,6 +58,7 @@ int ternarySearch(std::vector<int>& list, int target) {
   return -1;
 }
 
+//exponentially increases the step size to narrow down on a range of items, and when it finds that range, performs binary search
 int exponentialSearch(std::vector<int>& list, int target) {
   if (list.at(0)==target) {return 0;}
 
@@ -74,6 +78,7 @@ int exponentialSearch(std::vector<int>& list, int target) {
   return -1;
 }
 
+//steps in sqrt(the list's size) for minimal amount of steps without sacrificing efficiency.  it then performs linear search when the range is found.
 int jumpSearch(std::vector<int>& list, int target) {
   int root = (int)sqrt((double)list.size());
 
